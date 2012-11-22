adc6ccc339af928f07fe13252b23fe3e6baf7674
Damiaan van der Kruk <damiaan91@gmail.com>
2012-11-22 12:29:43 +0100

    Enum ECardType extended

--- bohnanza/src/main/ECardType.java
+++ bohnanza/src/main/ECardType.java
@@ -8,11 +8,93 @@ package main;
  *
  */
 public enum ECardType {
-	CHILLIBEAN  {
+	CHILIBEAN  {
 	    public String toString() {
-	        return "Chilli Bean";
+	        return "Chili Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 18;
 	    }
 	},
-	
+	COFFEEBEANS {
+		 public String toString() {
+		        return "Coffee Bean";
+		    }
+		    public int numberOfCards() {
+		    	return 24;
+		    }
+	},
+	WAXBEAN {
+		public String toString() {
+	        return "Wax Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 22;
+	    }
+	},
+	BLUEBEAN {
+		public String toString() {
+	        return "Blue Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 20;
+	    }
+	},
+	STICKBEAN {
+		public String toString() {
+	        return "Stick Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 16;
+	    }
+	},
+	GREENBEAN {
+		public String toString() {
+	        return "Green Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 14;
+	    }
+	},
+	SOYBEAN {
+		public String toString() {
+	        return "Soy Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 12;
+	    }
+	},
+	BLACKEYEDBEAN {
+		public String toString() {
+	        return "Black-eyed Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 10;
+	    }
+	},
+	REDBEAN {
+		public String toString() {
+	        return "Red Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 8;
+	    }
+	},
+	GARDENBEAN {
+		public String toString() {
+	        return "Garden Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 6;
+	    }
+	},
+	CACOABEAN {
+		public String toString() {
+	        return "Cocoa Bean";
+	    }
+	    public int numberOfCards() {
+	    	return 4;
+	    }
+	},
 
 }
