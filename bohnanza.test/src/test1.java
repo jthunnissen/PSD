dbd9b72a5669514f3cb43c4fd172987a0adc24c3
annuh <annevandevenis@gmail.com>
2012-11-22 15:21:41 +0100

    + Enkele tests

--- bohnanza.test/src/test1.java
+++ bohnanza.test/src/test1.java
@@ -1,5 +1,15 @@
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
+import main.BeanCard;
+import main.BeanField;
+import main.EBeanType;
+import main.Field;
+import main.Game;
+import main.GameFactory;
+import main.Player;
+
 import org.junit.Test;
 
 /**
@@ -11,10 +21,45 @@ import org.junit.Test;
  *
  */
 public class test1 {
+	
+	private static final String PLAYER_NAME = "Player";
+
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testPlayerInitScore(){
+		Player player = new Player(PLAYER_NAME);
+		assertSame("Score is not 0", player.getScore(), 0);
 	}
-
+	
+	@Test
+	public void testPlayerInitName(){
+		Player player = new Player(PLAYER_NAME);
+		assertEquals("Player has incorrect name", player.getName(), PLAYER_NAME);
+	}
+	
+	@Test
+	public void testCardType(){
+		BeanCard card = new BeanCard();
+		card.setType(EBeanType.BLACKEYEDBEAN);
+		assertSame("Card has incorrect type", card.getType(),EBeanType.BLACKEYEDBEAN);
+	}
+	
+	@Test
+	public void testFieldSetCard(){
+		BeanField field = new BeanField();
+		BeanCard card = new BeanCard();
+		field.addCard(card);
+		assertSame("Field has incorrect card", field.getCard().get(0),card);
+	}
+	
+	@Test
+	public void testGameInitPlayer(){
+		Player player1 = new Player(PLAYER_NAME);
+		Player player2 = new Player(PLAYER_NAME);
+		Game game = new Game(2);
+		ArrayList<Player> players = new ArrayList<Player>();
+		players.add(player1); players.add(player2);
+		game.setPlayers(players);
+		assertSame("Game has incorrect number of players", game.getPlayers().size(), players.size());
+	}
 }
